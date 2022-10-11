93850c9353cc27f6bad47fca7fd07111c52b0632
beforeold <ibeforeold@gmail.com>
2022-10-11 23:25:25 +0800

    add reverse string and unit tests

--- sources/RoadOfInterview/RoadOfInterview/ListDemo.swift
+++ sources/RoadOfInterview/RoadOfInterview/ListDemo.swift
@@ -23,4 +23,44 @@ struct ListDemo {
     reverse(&chars, start: 0, end: chars.count - 1)
     return String(chars)
   }
+  
+  func reverseWords(of string: String) -> String {
+    var chars = string.map { $0 }
+    var start = 0
+    
+    for i in 0..<chars.count {
+      if i == chars.count - 1 || chars[i + 1] == " " {
+        reverse(&chars, start: start, end: i)
+        start = i + 2
+      }
+    }
+    
+    return String(chars)
+  }
+}
+
+extension ListDemo {
+  static func test() {
+    do {
+      var array = [2, 4, 6]
+      ListDemo().reverse(&array, start: 0, end: array.count - 1)
+      assert(array == [6, 4, 2])
+    }
+    
+    do {
+      var array = [2, 4]
+      ListDemo().reverse(&array, start: 0, end: array.count - 1)
+      assert(array == [4, 2])
+    }
+    
+    do {
+      let ret = ListDemo().reverse(string: "it was monday")
+      assert(ret == "yadnom saw ti")
+    }
+    
+    do {
+      let ret = ListDemo().reverseWords(of: "it was monday")
+      assert(ret == "ti saw yadnom")
+    }
+  }
 }
--- sources/RoadOfInterview/RoadOfInterview/ViewController.swift
+++ sources/RoadOfInterview/RoadOfInterview/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+     
+    test()
+  }
+  
+  func test() {
+    ListDemo.test()
+    
+    print("all done")
+  }
+  
 }
 
