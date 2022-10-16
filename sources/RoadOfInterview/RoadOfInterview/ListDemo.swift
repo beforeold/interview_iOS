6f1bafb62cc83f6c7a4f923da783f272433413b5
beforeold <ibeforeold@gmail.com>
2022-10-16 21:17:03 +0800

    add extraElement(_:_:)

--- sources/RoadOfInterview/RoadOfInterview/ListDemo.swift
+++ sources/RoadOfInterview/RoadOfInterview/ListDemo.swift
@@ -64,3 +64,46 @@ extension ListDemo {
     }
   }
 }
+
+extension ListDemo {
+  // 给定两个整数数组A和B，A只比B多一个元素，其它元素都一样，找出多的那个元素
+  func extraElement(_ a1: [Int], _ a2: [Int]) -> Int {
+    let map1 = Dictionary(a1.map { ($0, 1) }, uniquingKeysWith: +)
+    let map2 = Dictionary(a1.map { ($0, 1) }, uniquingKeysWith: +)
+    
+    if map1.count == map2.count {
+      for (num, count) in map1 {
+        if count != map2[num] {
+          return num
+        }
+      }
+      return -1
+    } else {
+      let allKeys1 = Set(map1.keys)
+      let allKeys2 = Set(map2.keys)
+      let sub = allKeys1.subtracting(allKeys2)
+      return sub.first!
+    }
+  }
+  
+  func _2_extraElement(_ a1: [Int], _ a2: [Int]) -> Int {
+    var ret = [Int]()
+    
+    var set1 = Set(a1)
+    
+    for item in a2 {
+      if set1.contains(item) {
+        ret.append(item)
+        set1.remove(item)
+      }
+    }
+    
+    return ret.first!
+  }
+  
+  func _3_extraElement(_ a1: [Int], _ a2: [Int]) -> Int {
+    let set1 = Set(a1)
+    let set2 = Set(a2)
+    return set1.subtracting(set2).first!
+  }
+}
